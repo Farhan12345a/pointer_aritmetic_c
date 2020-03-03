beca66c80898f2d48f33f145a97ba4f707b21fe2
Farhan Shahbaz <49499937+Farhan12345a@users.noreply.github.com>
2020-03-02 22:09:22 -0500

    Add files via upload

---
+++ participation_09.c
@@ -0,0 +1,34 @@
+//Farhan Shahbaz
+
+//Pineville
+
+
+
+#include <stdio.h>
+
+
+
+int main()
+{
+
+    int arr[] = {2,3,4,5,7,54,200,65,78,99};
+
+    size_t size = sizeof(arr)/sizeof(arr[0]);
+
+    parameter_arith(arr,size);
+
+    return 0;
+
+}
+
+
+
+void parameter_arith(int *arr, int size){
+
+    for(int i = 0; i < size; i++){
+
+        printf("%d\n", *(arr + i));
+    
+}
+
+}
